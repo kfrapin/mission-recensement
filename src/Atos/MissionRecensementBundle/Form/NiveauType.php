d8411c8d6e1bd67ea18cf2d8b1d6e9a5c6afbdc7
kfrapin <kevin.frapin@gmail.com>
2014-02-27 11:13:12 +0100

    clean niveau views

--- src/Atos/MissionRecensementBundle/Form/NiveauType.php
+++ src/Atos/MissionRecensementBundle/Form/NiveauType.php
@@ -15,7 +15,9 @@ class NiveauType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+            ->add('nom', 'text', array(
+                'label' => 'Niveau',
+            ))
         ;
     }
     
