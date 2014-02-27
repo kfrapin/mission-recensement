75b38f8aa9569659520a92033e6b5764873fc051
kfrapin <kevin.frapin@gmail.com>
2014-02-27 11:17:17 +0100

    clean specialite metier views

--- src/Atos/MissionRecensementBundle/Form/SpecialiteMetierType.php
+++ src/Atos/MissionRecensementBundle/Form/SpecialiteMetierType.php
@@ -15,7 +15,9 @@ class SpecialiteMetierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+            ->add('nom', 'text', array(
+                'label' => 'Spécialité métier',
+            ))
         ;
     }
     
